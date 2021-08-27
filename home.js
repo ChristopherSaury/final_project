e54f1e77e868ab2beb7420aad9c9545e5cc79a75
ChristopherSaury <saurychristopher@gmail.com>
2021-08-27 13:02:38 +0200

    initial version

--- home.js
+++ home.js
@@ -1,3 +1,51 @@
+//NAVBAR
+// Récupération des blocs
+let mainMenu = document.querySelector("#menu");
+let burgerMenu = document.querySelector("#menu-burger");
+let closingDivMenu = document.querySelector(".closing-bkg");
+
+burgerMenu.addEventListener("click", function(){
+	console.log("clicked");
+	if (mainMenu.getAttribute("class") != "visible" && closingDivMenu.getAttribute != "bkg-active"){
+		mainMenu.setAttribute("class", "visible");
+		closingDivMenu.setAttribute("class", "bkg-active");
+	}else{
+		mainMenu.setAttribute("class", "invisible");
+		burgerMenu.setAttribute("class", "closing-bkg");
+	}
+})
+
+closingDivMenu.addEventListener("click", function(){
+	console.log("bkg clicked");
+	if(closingDivMenu.getAttribute("class") != "closing-bkg" && mainMenu.getAttribute("class") != "invisible"){
+		closingDivMenu.setAttribute("class", "closing-bkg");
+		mainMenu.setAttribute("class", "invisible");
+	}
+	
+})
+
+// closingDivMenu.addEventListener(clickedEvent, function(evt){
+// 	console.log(clickedEvent);
+// 	if(!this.getAttribute("class")) {
+// 		this.setAttribute("class", "clicked");
+// 	} else {
+// 		this.removeAttribute("class");
+// 	}
+	
+
+// }, false)
+
+
+
+
+
+
+
+
+
+
+//SLIDESHOW
+
 // on commence par sélectionner les class .home-slideshow (le containeur des images) et
 //.home-slideshow-image qui permet de sélectionner toutes les images
 
@@ -7,7 +55,7 @@ const slideshowImages = document.querySelectorAll(
 
 // On stock dans des variables le délai avant chaque transition et une valeurs index à
 // partir de 0 qui va nous permettre de commencer à  partir la première image
-const nextImageDelay = 6000;
+const nextImageDelay = 4000;
 let currentImageCounter = 0;
 
 // on change l'opacité de 0 à 1 pour chaque transition pour faire apparaître une nouvelle image
@@ -22,7 +70,8 @@ function nextImage() {
   slideshowImages[currentImageCounter].style.opacity = 1;
 }
 
-// Partie reveal on scroll
+// PARTIE REVEAL ON SCROLL
+
 // On commence par ajouter un listener qui va écouter le scroll sur la page puis appeler la fonction reveal
 window.addEventListener("scroll", reveal);
 
