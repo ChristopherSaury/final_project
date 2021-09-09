9a1be6aab77869c3dce2194a730a8c1359d2f583
ChristopherSaury <saurychristopher@gmail.com>
2021-09-09 14:26:10 +0200

    constants have added

--- config.php
+++ config.php
@@ -6,3 +6,7 @@ const MODELS = __DIR__ . '/src/models';
 const VIEWS = __DIR__ . '/views';
 
 const BASE_URL = 'http://localhost/projet-titre/final_project';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PSW = '';
+const DB_NAME = 'cafeteria_db';
