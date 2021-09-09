1ed07943d6633b3ef7f615ce7458703cd80bd99f
ChristopherSaury <saurychristopher@gmail.com>
2021-09-09 14:29:10 +0200

    routes have been modified

--- index.php
+++ index.php
@@ -12,27 +12,44 @@ if(!empty($_GET['route'])){
     switch($route){
         case 'accueil':
         require CONTROLLERS . '/home_controller.php';
+        init_session();
         display_home();
         break;
 
         case 'connexion':
         require CONTROLLERS . '/login_controller.php';
+        init_session();
         display_login_page();
+        break;
+
+        case 'connexion-handler':
+        require CONTROLLERS . '/login_controller.php';
+        init_session();
+        login_handler();
         break;
 
         case 'inscription':
         require CONTROLLERS . '/sign_up_controller.php';
         display_signup_page();
+        init_session();
+        break;
+
+        case 'inscription-handler':
+        require CONTROLLERS . '/sign_up_controller.php';
+        create_user_handler();
+        init_session();
         break;
 
         case 'selection':
         require CONTROLLERS . '/selection_page_controller.php';
         display_selection_page();
+        init_session();
         break;
 
         case 'contact':
         require CONTROLLERS . '/contact_controller.php';
         display_contact_page();
+        init_session();
         break;
     }
 }else {
